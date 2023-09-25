087a6ae0e33e498de1e58422dec6111271a1e9ac
haiiro2gou <mrashen.2gou@gmail.com>
2023-09-26 01:55:54 +0900

    fixed boot shells

--- proxy/boot.sh
+++ proxy/boot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 cd ../
-java/jdk-17.0.8+7/bin/java -jar -Xmx512M -Xms512M velocity.jar nogui
+java/jdk-17.0.8+7/bin/java -jar -Xmx512M -Xms512M proxy/velocity.jar nogui
