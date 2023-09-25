3546202d8868b81762c7e32b9d26cd0d628f866a
haiiro2gou <mrashen.2gou@gmail.com>
2023-09-26 01:58:07 +0900

    fixed boot scripts

--- proxy/boot.sh
+++ proxy/boot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 cd ../
-java/jdk-17.0.8+7/bin/java -jar -Xmx512M -Xms512M proxy/velocity.jar nogui
+screen -UAmdS velocity java/jdk-17.0.8+7/bin/java -jar -Xmx512M -Xms512M proxy/velocity.jar nogui
