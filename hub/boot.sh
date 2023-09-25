216735edc6de6b92db458a6ea08a8e938209115b
haiiro2gou <mrashen.2gou@gmail.com>
2023-09-26 00:40:38 +0900

    fix boot shells

--- hub/boot.sh
+++ hub/boot.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-exec ../java/jdk-16/bin/java -jar @user_jvm_args.txt paper.jar nogui
+cd ../
+exec java/jdk-16/bin/java -jar @user_jvm_args.txt paper.jar nogui
--- proxy/boot.sh
+++ proxy/boot.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-exec ../java/jdk-17.0.1/bin/java -jar @user_jvm_args.txt velocity.jar nogui
+cd ../
+exec java/jdk-17.0.1/bin/java -jar @user_jvm_args.txt velocity.jar nogui
