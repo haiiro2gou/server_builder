e373683cfa8b4e0a24e35c30b43c808576e8b725
haiiro2gou <mrashen.2gou@gmail.com>
2023-10-01 12:10:17 +0900

    updated some batch files

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 @echo off
 title Server Initializer for Windows
 powershell -File init_script.ps1
-pause
+exit
--- proxy/boot.bat
+++ proxy/boot.bat
@@ -1,4 +1,4 @@
 @echo off
 title Velocity
 "../java/jdk-17/bin/java.exe" -jar @user_jvm_args.txt velocity.jar
-pause
+exit
