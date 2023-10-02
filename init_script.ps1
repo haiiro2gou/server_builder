658ab178f6d18a72590ff274e0b11a22221156de
haiiro2gou <mrashen.2gou@gmail.com>
2023-10-03 00:17:15 +0900

    added plugins

--- init_script.ps1
+++ init_script.ps1
@@ -1,5 +1,9 @@
 # java
 New-Item java -ItemType Directory
+# java 8
+Invoke-WebRequest https://objects.githubusercontent.com/github-production-release-asset-2e65be/372924428/64302593-59eb-4746-b3ca-33985e5da373?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20231002%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231002T151320Z&X-Amz-Expires=300&X-Amz-Signature=1189bd44adc23154a6ef3dc6cd52476509e9bbf70648211ff895939165526fdb&X-Amz-SignedHeaders=host&actor_id=99890591&key_id=0&repo_id=372924428&response-content-disposition=attachment%3B%20filename%3DOpenJDK8U-jdk_x64_windows_hotspot_8u382b05.zip&response-content-type=application%2Foctet-stream
+Expand-Archive -Path OpenJDK8U-jdk_x64_windows_hotspot_8u382b05.zip -DestinationPath java/
+Remove-Item -Path OpenJDK8U-jdk_x64_windows_hotspot_8u382b05.zip
 # java 16
 Invoke-WebRequest "https://download.java.net/openjdk/jdk16/ri/openjdk-16+36_windows-x64_bin.zip" -O jdk-16.zip
 Expand-Archive -Path jdk-16.zip -DestinationPath java/
@@ -13,6 +17,7 @@ Remove-Item -Path jdk-17.zip
 Invoke-WebRequest https://api.papermc.io/v2/projects/velocity/versions/3.2.0-SNAPSHOT/builds/265/downloads/velocity-3.2.0-SNAPSHOT-265.jar -O proxy/velocity.jar
 # velocity plugins
 Invoke-WebRequest https://github.com/adde0109/Ambassador/releases/download/v1.4.1-beta/Ambassador-Velocity-1.4.1-all.jar -O proxy/plugins/ambassador.jar
+Invoke-WebRequest https://github.com/ygtdmn/VelocityRcon/releases/download/v1.1/VelocityRcon.jar -O proxy/plugins/velocity_rcon.jar
 
 # papermc server
 Invoke-WebRequest https://api.papermc.io/v2/projects/paper/versions/1.16.5/builds/794/downloads/paper-1.16.5-794.jar -O hub/paper.jar
--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 cd "$(dirname "$0")"
 mkdir java
 
+# java 8
+wget -c https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u352-b08/OpenJDK8U-jdk_aarch64_linux_hotspot_8u352b08.tar.gz
+tar xvf OpenJDK8U-jdk_aarch64_linux_hotspot_8u352b08.tar.gz
+sudo mv jdk8u352-b08 java/
+
 # java 16
 wget -c https://download.visualstudio.microsoft.com/download/pr/47631081-c93a-4629-be38-bec6fced2c35/84d7a8cd3efb467f991207d3961f65ae/microsoft-jdk-16.0.2.7.1-linux-aarch64.tar.gz
 tar xvf microsoft-jdk-16.0.2.7.1-linux-aarch64.tar.gz
@@ -15,6 +20,7 @@ sudo mv jdk-17.0.8+7 java/
 wget -c https://api.papermc.io/v2/projects/velocity/versions/3.2.0-SNAPSHOT/builds/265/downloads/velocity-3.2.0-SNAPSHOT-265.jar -O proxy/velocity.jar
 # velocity plugins
 wget -c https://github.com/adde0109/Ambassador/releases/download/v1.4.1-beta/Ambassador-Velocity-1.4.1-all.jar -O proxy/plugins/ambassador.jar
+wget -c https://github.com/ygtdmn/VelocityRcon/releases/download/v1.1/VelocityRcon.jar -O proxy/plugins/velocity_rcon.jar
 
 # papermc server
 wget -c https://api.papermc.io/v2/projects/paper/versions/1.16.5/builds/794/downloads/paper-1.16.5-794.jar -O hub/paper.jar
