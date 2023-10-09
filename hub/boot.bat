d8291bc96a614eaa8aa031372b8f0792aebc4439
haiiro2gou <mrashen.2gou@gmail.com>
2023-10-10 00:29:31 +0900

    updated java version

--- hub/boot.bat
+++ hub/boot.bat
@@ -1,4 +1,5 @@
 @echo off
 title Universal Hub
-"../java/jdk-16/bin/java.exe" -jar @user_jvm_args.txt paper.jar nogui
+"../java/jdk-17/bin/java.exe" -jar @user_jvm_args.txt paper.jar nogui
+pause
 exit
